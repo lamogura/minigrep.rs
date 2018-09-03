48291a489f620e97ac9212550ecee9a8a94080a4
Justin Kovalchuk <xentropic@gmail.com>
2018-09-03 13:20:01 +0900

    use iterators

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,19 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("not enough arguments");
-    }
+  pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+    args.next();
 
-    let query = args[1].clone();
-    let filename = args[2].clone();
+    let query = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Did't get a query string"),
+    };
+
+    let filename = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a file name"),
+    };
+
     let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
     Ok(Config {
@@ -29,27 +35,18 @@ impl Config {
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   let query = query.to_lowercase();
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.to_lowercase().contains(&query) {
-      results.push(line);
-    }
-  }
-
-  results
+  contents
+    .lines()
+    .map(|line| line.to_lowercase())
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-
-  results
+  contents
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
